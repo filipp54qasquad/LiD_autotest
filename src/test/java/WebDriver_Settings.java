03b42d04cabba7a837a579b2788adac2ca862925
QASQUAD <popok.f@qasquad.ru>
2021-03-26 10:59:07 +0700

    add

---
+++ src/test/java/WebDriver_Settings.java
@@ -0,0 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriver_Settings {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+   // @After
+   // public void close() {
+     //   driver.quit();
+   // }
+}
+
